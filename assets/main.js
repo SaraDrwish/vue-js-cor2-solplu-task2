6e5a26cb04f8d56cc9d083a32267d5aacbe29de7
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2024-03-13 03:50:55 +0300

    4 log 1-2

--- assets/main.js
+++ assets/main.js
@@ -7,10 +7,20 @@ console.log("hi main . js ");
 
 let imgSlidr = document.querySelector(".img-slidr");
 let imgsArr = ["1.png", "2.png", "3.png", "4.png"];
+let randbtn = document.querySelector(".random");
+imgSlidr.style.backgroundImage = 'url("./imgs/1.png")';
 
-imgSlidr.style.backgroundImage = 'url("imgs/1.png")';
+// let randImg = Math.floor(Math.random() * imgsArr.length);
+// console.log(randImg);
 
-let randImg = Math.floor(Math.random() * imgsArr.length);
-console.log(randImg);
+randbtn.addEventListener("click", () => {
+  setInterval(() => {
+    let randImg = Math.floor(Math.random() * imgsArr.length);
+    imgSlidr.style.backgroundImage =
+      'url("./imgs/' + imgsArr[randImg] + '.png")';
+
+    console.log(randImg);
+  }, 1000);
+});
 
 // end
