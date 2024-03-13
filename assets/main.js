79f1f225d3f2a4bea906de2d12da4535542ec700
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2024-03-13 03:38:15 +0300

    4 log1

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,16 @@
 console.log("hi main . js ");
+//1
+
+//////end
+
+// t2
+
+let imgSlidr = document.querySelector(".img-slidr");
+let imgsArr = ["1.png", "2.png", "3.png", "4.png"];
+
+imgSlidr.style.backgroundImage = 'url("imgs/1.png")';
+
+let randImg = Math.floor(Math.random() * imgsArr.length);
+console.log(randImg);
+
+// end
