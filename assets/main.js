43cbf3ba6f44509c561919e1d8d2440d3fb29158
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2024-03-13 04:00:58 +0300

    5 log 1-2 /// 1 1

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,16 @@
 console.log("hi main . js ");
 //1
 
+setInterval(() => {
+  var box1 = document.querySelector(".box1");
+  var width = Math.floor(Math.random() * 20) + 5; // Random width between 50px and 250px
+  var height = Math.floor(Math.random() * 20) + 5; // Random height between 50px and 250px
+  var randomColor = "#" + ((Math.random() * 0xffffff) << 0).toString(16); // Random color
+
+  box1.style.width = width + "rem";
+  box1.style.height = height + "rem";
+  box1.style.backgroundColor = randomColor;
+}, 1000);
 //////end
 
 // t2
@@ -8,8 +18,9 @@ console.log("hi main . js ");
 let imgSlidr = document.querySelector(".img-slidr");
 let imgsArr = ["1.png", "2.png", "3.png", "4.png"];
 let randbtn = document.querySelector(".random");
+console.log(imgSlidr);
 imgSlidr.style.backgroundImage = 'url("./imgs/1.png")';
-
+console.log(imgSlidr);
 // let randImg = Math.floor(Math.random() * imgsArr.length);
 // console.log(randImg);
 
