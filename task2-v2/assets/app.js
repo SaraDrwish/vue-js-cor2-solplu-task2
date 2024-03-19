367434ad4da4ddf83f232150382c819adb46d649
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2024-03-20 01:57:13 +0300

    pl -  task week 3 - v9

--- task2-v2/assets/app.js
+++ task2-v2/assets/app.js
@@ -1,14 +1,14 @@
-let imgArr = [
-  "1.png",
-  "2.png",
-  "3.png",
-  "4.png",
-  "5.png",
-  "6.png",
-  "7.png",
-  "8.png",
-  "9.png",
-];
+// let imgArr = [
+//   "1.png",
+//   "2.png",
+//   "3.png",
+//   "4.png",
+//   "5.png",
+//   "6.png",
+//   "7.png",
+//   "8.png",
+//   "9.png",
+// ];
 
 let randImgBtn = document.querySelector(".random");
 let autoImgBtn = document.querySelector(".auto");
@@ -16,22 +16,19 @@ let prevImgBtn = document.querySelector(".prev");
 let nextImgBtn = document.querySelector(".next");
 let stopImgBtn = document.querySelector(".stop");
 // ///////////
-// let apiTitle = document.querySelector(".img-slider-h3-title");
-// let apiPost = document.querySelector(".img-slider-p");
-// let apispanNum = document.querySelector(".apiContent span");
+let apiTitle = document.querySelector(".img-slider-h3-title");
+let apiPost = document.querySelector(".img-slider-p");
+let apispanNum = document.querySelector(".apiContent span");
 let apiContent = document.querySelector(".apiContent");
 // //////////////////
-
-let apiTitle = document.createElement("h3");
-let apiPost = document.createElement("p");
-let apispanNum = document.createElement("span");
-// let apiContent = document.createElement("div");
-
-apiTitle.className = "img-slider-h3-title";
-apiPost.className = "img-slider-p";
-apispanNum.className = "span";
+// let apiTitle = document.createElement("h3");
+// let apiPost = document.createElement("p");
+// let apispanNum = document.createElement("span");
+// apiTitle.className = "img-slider-h3-title";
+// apiPost.className = "img-slider-p";
+// apispanNum.className = "span";
 // apiContent.className = "apiContent";
-
+// ////////////////////
 // //////////// start task week 3 ---------
 let apiData;
 let apiCount = 0;
@@ -44,87 +41,85 @@ fetch("https://jsonplaceholder.typicode.com/posts")
     console.log("data is : (array) ");
     console.log(data);
     apiData = data.forEach((el) => {
-      apiTitle.innerHTML = `<span>${el.id}</span>`;
+      apiTitle = `<span>${el.id}</span>`;
       let apih3 = `<h3>${el.title}</h3>`;
-      //   let apih3 = `<h3>${el.title}</h3>`;
       let apiP = `<p>${el.body}</p>`;
 
-      //   let apiSpanElm = document
-      //     .querySelector(".apiContent")
-      //     .insertAdjacentHTML("beforeend", apiSpan);
-      //   let apih3Elm = document.querySelector(".apiContent");
-      //   apih3Elm.insertAdjacentHTML("beforeend", apih3);
-      //   let apiPElm = document.querySelector(".apiContent").insertAdjacentHTML("beforeend", apiP);
+      let apiSpanElm = document
+        .querySelector(".apiContent")
+        .insertAdjacentHTML("beforeend", apiP);
+      let apih3Elm = document
+        .querySelector(".apiContent")
+        .insertAdjacentHTML("beforeend", apih3);
 
-      //   apiContent.innerHTML = `
-      //    let apiSpan = <span>${el.id}</span>
-      //    <h3>${el.title}</h3>
-      //    <p>${el.body}</p>
-      //     `;
-
-      apiContent = `${apiTitle}`;
-      apiContent.innerHTML = apiTitle;
-      //   apiContent.insertAdjacentHTML("beforeend", apiTitle);
+      let apiTitleElm = document
+        .querySelector(".apiContent")
+        .insertAdjacentHTML("beforeend", apiTitle);
 
       console.log(el);
       console.log(el.body + "   --  " + el.id + "   --  " + el.title);
       // ////////////
 
-      let autoPlay;
-
-      function getRandomImg() {
-        function randomImg() {
-          let randapi = Math.floor(Math.random() * el.id + 1);
-          console.log(randapi);
-          apiContentContainer.insertAdjacentHTML("afterend", randapi);
-        }
-
-        autoPlay = setInterval(randomImg, 900);
-      }
-
-      let currentImg = 0;
-
-      function autoDisplayImg() {
-        autoPlay = setInterval(() => {
-          if (currentImg >= imgArr.length - 1) {
-            currentImg = -1;
-          }
-          currentImg++;
-          return setImgUI();
-        }, 1000);
-      }
-
-      function stopAutoPlayImg() {
-        clearInterval(autoPlay);
-      }
-
-      function setImgUI() {
-        return (imageBox.src = `./assets/imgs/${imgArr[currentImg]}`);
-      }
-
-      function nextImgSelct() {
-        if (currentImg >= imgArr.length - 1) {
-          currentImg = -1;
-        }
-        currentImg++;
-        return setImgUI();
-      }
-
-      function prevImgSelct() {
-        if (currentImg <= 0) {
-          currentImg = imgArr.length;
-        }
-        currentImg--;
-        return setImgUI();
-      }
-      randImgBtn.addEventListener("click", getRandomImg);
-      autoImgBtn.addEventListener("click", autoDisplayImg);
-      stopImgBtn.addEventListener("click", stopAutoPlayImg);
-      nextImgBtn.addEventListener("click", nextImgSelct);
-      prevImgBtn.addEventListener("click", prevImgSelct);
       // ///////////
     });
   })
   .catch((error) => console.log("error is : " + error));
+
+//   //////////////////
+
+let autoPlay;
+
+function getRandomImg() {
+  function randomImg() {
+    let randapi = Math.floor(Math.random() * el.id + 1);
+    console.log(randapi);
+    apiContentContainer.insertAdjacentHTML("afterend", randapi);
+  }
+
+  autoPlay = setInterval(randomImg, 900);
+}
+
+let currentImg = 0;
+
+function autoDisplayImg() {
+  autoPlay = setInterval(() => {
+    if (currentImg >= imgArr.length - 1) {
+      currentImg = -1;
+    }
+    currentImg++;
+    return setImgUI();
+  }, 1000);
+}
+
+function stopAutoPlayImg() {
+  clearInterval(autoPlay);
+}
+
+function setImgUI() {
+  return (imageBox.src = `./assets/imgs/${imgArr[currentImg]}`);
+}
+
+function nextImgSelct() {
+  if (currentImg >= imgArr.length - 1) {
+    currentImg = -1;
+  }
+  currentImg++;
+  return setImgUI();
+}
+
+function prevImgSelct() {
+  if (currentImg <= 0) {
+    currentImg = imgArr.length;
+  }
+  currentImg--;
+  return setImgUI();
+}
+randImgBtn.addEventListener("click", getRandomImg);
+autoImgBtn.addEventListener("click", autoDisplayImg);
+stopImgBtn.addEventListener("click", stopAutoPlayImg);
+nextImgBtn.addEventListener("click", nextImgSelct);
+prevImgBtn.addEventListener("click", prevImgSelct);
+
+// ////////////////
 
 // //////////// end task week 3 ---------
