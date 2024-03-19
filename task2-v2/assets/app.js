787b72ea7a38cb74b0d05b1ff87d6fb8bd9fc0ee
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2024-03-20 00:30:28 +0300

    pl -  task week 3 - v5

--- task2-v2/assets/app.js
+++ task2-v2/assets/app.js
@@ -16,23 +16,39 @@ let prevImgBtn = document.querySelector(".prev");
 let nextImgBtn = document.querySelector(".next");
 let stopImgBtn = document.querySelector(".stop");
 // ///////////
-let apiTitle = document.querySelector(".img-slider-h3-title");
-let apiPost = document.querySelector(".img-slider-p");
-let apispanNum = document.querySelector(".apiContent span");
+// let apiTitle = document.querySelector(".img-slider-h3-title");
+// let apiPost = document.querySelector(".img-slider-p");
+// let apispanNum = document.querySelector(".apiContent span");
 // //////////// start task week 3 ---------
-
+let apiData;
+let apiCount = 0;
 fetch("https://jsonplaceholder.typicode.com/posts")
   .then((res) => {
     console.log("the res is : " + res);
     return res.json();
   })
   .then((data) => {
-    console.log("the data is : " + data);
-    data.forEach((el) => {
-      console.log("the el is : " + el);
-      apispanNum.textContent = `${el.id}`;
-      apiTitle.textContent = `${"Tilte is : " + el.title}`;
-      apiPost.textContent = `${"The Post is : " + el.body}`;
+    console.log("data is : (array) ");
+    console.log(data);
+    apiData = data.forEach((el) => {
+      //   console.log(el);
+      let apiSpan = `<span>${el.id}</span>`;
+      let apih3 = `<h3>${el.title}</h3>`;
+      let apiP = `<p>${el.body}</p>`;
+      document
+        .querySelector(".apiContent")
+        .insertAdjacentHTML("beforeend", apiSpan);
+      document
+        .querySelector(".apiContent")
+        .insertAdjacentHTML("beforeend", apih3);
+      document
+        .querySelector(".apiContent")
+        .insertAdjacentHTML("beforeend", apiP);
+
+      //   let apiSpanElmnt = document.querySelector(".apiContent").innerHTML(apiSpan);
+
+      console.log(el.body + "   --  " + el.id + "   --  " + el.title);
+      console.log(el);
     });
   })
   .catch((error) => console.log("error is : " + error));
