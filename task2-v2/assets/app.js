75e4e1959d70f66083718668f3ea9c8aabebd2e3
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2024-03-20 01:34:39 +0300

    pl -  task week 3 - v7

--- task2-v2/assets/app.js
+++ task2-v2/assets/app.js
@@ -32,94 +32,94 @@ fetch("https://jsonplaceholder.typicode.com/posts")
     console.log("data is : (array) ");
     console.log(data);
     apiData = data.forEach((el) => {
-      //   console.log(typeof el);
-      //   let count = 0;
-      //   for (let elm in el) {
-      // if (el.hasOwnProperty(el.title)) {
+      let apiSpan = `<span>${el.id}</span>`;
+      let apih3 = `<h3>${el.title}</h3>`;
+      let apiP = `<p>${el.body}</p>`;
 
-      apiContent = `
-        <span>${el.id}</span>
-        <h3 class="img-slider-h3-title">${el.title}</h3>
-        <p class="img-slider-p">${el.body}</p>`;
-      //   apiTitle.textContent = `${el.title}`;
-      //   apiPost.textContent = `${el.body}`;
-      //   apispanNum.textContent = `${el.id}`;
-      // }
-      // count++;
-      //   }
-      //   let apiSpan = `<span>${el.id}</span>`;
-      //   let apih3 = `<h3>${el.title}</h3>`;
-      //   let apiP = `<p>${el.body}</p>`;
-      //   document
-      //     .querySelector(".apiContent")
-      //     .insertAdjacentHTML("beforeend", apiSpan);
-      //   document
-      //     .querySelector(".apiContent")
-      //     .insertAdjacentHTML("beforeend", apih3);
-      //   document
-      //     .querySelector(".apiContent")
-      //     .insertAdjacentHTML("beforeend", apiP);
+      let apiSpanElm = document
+        .querySelector(".apiContent")
+        .insertAdjacentHTML("beforeend", apiSpan);
+      let apih3Elm = document
+        .querySelector(".apiContent")
+        .insertAdjacentHTML("beforeend", apih3);
+      let apiPElm = document
+        .querySelector(".apiContent")
+        .insertAdjacentHTML("beforeend", apiP);
 
-      console.log(el.body + "   --  " + el.id + "   --  " + el.title);
+      //   let apiContentContainer = document.querySelector(".apiContent-container");
+      //   apiContent = apiSpanElm + apih3Elm + apiPElm;
+      //   apiContentContainer.insertAdjacentHTML("afterend", apiContent);
+
+      //   apiContent.insertAdjacentHTML("beforeend", apiSpanElm, apih3Elm, apiPElm);
+
+      //   apiContent.appendChild(apiSpanElm, apih3Elm, apiPElm);
+      //   apiContent.appendChild("apiSpanElm");
+      //   apiContent.appendChild("apih3Elm");
+      //   apiContent.appendChild("apiPElm");
+
+      apiContent.appendChild(apiSpanElm);
+      apiContent.appendChild(apih3Elm);
+      apiContent.appendChild(apiPElm);
+
       console.log(el);
+      console.log(el.body + "   --  " + el.id + "   --  " + el.title);
+      // ////////////
+
+      let autoPlay;
+
+      function getRandomImg() {
+        function randomImg() {
+          let randapi = Math.floor(Math.random() * el.id + 1);
+          console.log(randapi);
+          apiContentContainer.insertAdjacentHTML("afterend", randapi);
+        }
+
+        autoPlay = setInterval(randomImg, 900);
+      }
+
+      let currentImg = 0;
+
+      function autoDisplayImg() {
+        autoPlay = setInterval(() => {
+          if (currentImg >= imgArr.length - 1) {
+            currentImg = -1;
+          }
+          currentImg++;
+          return setImgUI();
+        }, 1000);
+      }
+
+      function stopAutoPlayImg() {
+        clearInterval(autoPlay);
+      }
+
+      function setImgUI() {
+        return (imageBox.src = `./assets/imgs/${imgArr[currentImg]}`);
+      }
+
+      function nextImgSelct() {
+        if (currentImg >= imgArr.length - 1) {
+          currentImg = -1;
+        }
+        currentImg++;
+        return setImgUI();
+      }
+
+      function prevImgSelct() {
+        if (currentImg <= 0) {
+          currentImg = imgArr.length;
+        }
+        currentImg--;
+        return setImgUI();
+      }
+      randImgBtn.addEventListener("click", getRandomImg);
+      autoImgBtn.addEventListener("click", autoDisplayImg);
+      stopImgBtn.addEventListener("click", stopAutoPlayImg);
+      nextImgBtn.addEventListener("click", nextImgSelct);
+      prevImgBtn.addEventListener("click", prevImgSelct);
+      // ///////////
     });
   })
   .catch((error) => console.log("error is : " + error));
 
 // //////////// end task week 3 ---------
-
-let autoPlay;
-
-let imageBox = document.querySelector(".img-slider-img");
-
-function getRandomImg() {
-  function randomImg() {
-    let randImg = Math.floor(Math.random() * imgArr.length + 1);
-    console.log(randImg);
-    imageBox.src = `./assets/imgs/${randImg}.png`;
-  }
-
-  autoPlay = setInterval(randomImg, 900);
-}
-
-let currentImg = 0;
-
-function autoDisplayImg() {
-  autoPlay = setInterval(() => {
-    if (currentImg >= imgArr.length - 1) {
-      currentImg = -1;
-    }
-    currentImg++;
-    return setImgUI();
-  }, 1000);
-}
-
-function stopAutoPlayImg() {
-  clearInterval(autoPlay);
-}
-
-function setImgUI() {
-  return (imageBox.src = `./assets/imgs/${imgArr[currentImg]}`);
-}
-
-function nextImgSelct() {
-  if (currentImg >= imgArr.length - 1) {
-    currentImg = -1;
-  }
-  currentImg++;
-  return setImgUI();
-}
-
-function prevImgSelct() {
-  if (currentImg <= 0) {
-    currentImg = imgArr.length;
-  }
-  currentImg--;
-  return setImgUI();
-}
-
-randImgBtn.addEventListener("click", getRandomImg);
-autoImgBtn.addEventListener("click", autoDisplayImg);
-stopImgBtn.addEventListener("click", stopAutoPlayImg);
-nextImgBtn.addEventListener("click", nextImgSelct);
-prevImgBtn.addEventListener("click", prevImgSelct);
