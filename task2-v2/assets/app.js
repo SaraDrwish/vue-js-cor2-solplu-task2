fdee9c5d39baf1989537a7cd7e548edd8bb13207
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2024-03-20 23:00:43 +0300

    pl -  task week 3 - 20-03-2024 nigh 1

--- task2-v2/assets/app.js
+++ task2-v2/assets/app.js
@@ -1,70 +1,54 @@
-// let imgArr = [
-//   "1.png",
-//   "2.png",
-//   "3.png",
-//   "4.png",
-//   "5.png",
-//   "6.png",
-//   "7.png",
-//   "8.png",
-//   "9.png",
-// ];
-
 let randImgBtn = document.querySelector(".random");
 let autoImgBtn = document.querySelector(".auto");
 let prevImgBtn = document.querySelector(".prev");
 let nextImgBtn = document.querySelector(".next");
 let stopImgBtn = document.querySelector(".stop");
-// ///////////
+
 let apiTitle = document.querySelector(".img-slider-h3-title");
 let apiPost = document.querySelector(".img-slider-p");
 let apispanNum = document.querySelector(".apiContent span");
 let apiContent = document.querySelector(".apiContent");
-// //////////////////
-// let apiTitle = document.createElement("h3");
-// let apiPost = document.createElement("p");
-// let apispanNum = document.createElement("span");
-// apiTitle.className = "img-slider-h3-title";
-// apiPost.className = "img-slider-p";
-// apispanNum.className = "span";
-// apiContent.className = "apiContent";
-// ////////////////////
+
 // //////////// start task week 3 ---------
 let apiData;
 let apiCount = 0;
-fetch("https://jsonplaceholder.typicode.com/posts")
-  .then((res) => {
-    console.log("the res is : " + res);
-    return res.json();
-  })
-  .then((data) => {
-    console.log("data is : (array) ");
-    console.log(data);
-    apiData = data.forEach((el) => {
-      apiTitle = `<span>${el.id}</span>`;
-      let apih3 = `<h3>${el.title}</h3>`;
-      let apiP = `<p>${el.body}</p>`;
 
-      let apiSpanElm = document
-        .querySelector(".apiContent")
-        .insertAdjacentHTML("beforeend", apiP);
-      let apih3Elm = document
-        .querySelector(".apiContent")
-        .insertAdjacentHTML("beforeend", apih3);
+let handlResponsFetch = () => {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => {
+      console.log("the res is : " + res);
+      return res.json();
+    })
+    .then((data) => {
+      console.log("data is : (array) ");
+      console.log(data);
+      apiData = data.forEach((el) => {
+        apiTitle = `<span>${el.id}</span>`;
+        let apih3 = `<h3>${el.title}</h3>`;
+        let apiP = `<p>${el.body}</p>`;
 
-      let apiTitleElm = document
-        .querySelector(".apiContent")
-        .insertAdjacentHTML("beforeend", apiTitle);
+        let apiSpanElm = document
+          .querySelector(".apiContent")
+          .insertAdjacentHTML("beforeend", apiP);
+        let apih3Elm = document
+          .querySelector(".apiContent")
+          .insertAdjacentHTML("beforeend", apih3);
 
-      console.log(el);
-      console.log(el.body + "   --  " + el.id + "   --  " + el.title);
-      // ////////////
+        let apiTitleElm = document
+          .querySelector(".apiContent")
+          .insertAdjacentHTML("beforeend", apiTitle);
 
-      // ///////////
-    });
-  })
-  .catch((error) => console.log("error is : " + error));
+        console.log(el);
+        console.log(el.body + "   --  " + el.id + "   --  " + el.title);
+        // ////////////
 
+        // ///////////
+      });
+    })
+    .catch((error) => console.log("error is : " + error));
+};
+
+handlResponsFetch();
 //   //////////////////
 
 let autoPlay;
