dbbbc7b357283d3cc758323027a6857001a3f0ca
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2024-03-21 14:48:41 +0300

    merg tow tasks

--- task2-v2/assets/app.js
+++ task2-v2/assets/app.js
@@ -1,15 +1,81 @@
+// ///////
+let imgArr = [
+  "1.png",
+  "2.png",
+  "3.png",
+  "4.png",
+  "5.png",
+  "6.png",
+  "7.png",
+  "8.png",
+  "9.png",
+];
 let randImgBtn = document.querySelector(".random");
 let autoImgBtn = document.querySelector(".auto");
 let prevImgBtn = document.querySelector(".prev");
 let nextImgBtn = document.querySelector(".next");
 let stopImgBtn = document.querySelector(".stop");
 
+let autoPlay;
+let imageBox = document.querySelector(".img-slider-img");
+let currentImg = 0;
+
+function getRandomImg() {
+  function randomImg() {
+    let randImg = Math.floor(Math.random() * imgArr.length + 1);
+    console.log(randImg);
+    imageBox.src = `./assets/imgs/${randImg}.png`;
+  }
+  autoPlay = setInterval(randomImg, 900);
+}
+function autoDisplayImg() {
+  autoPlay = setInterval(() => {
+    if (currentImg >= imgArr.length - 1) {
+      currentImg = -1;
+    }
+    currentImg++;
+    return setImgUI();
+  }, 1000);
+}
+function stopAutoPlayImg() {
+  clearInterval(autoPlay);
+}
+function setImgUI() {
+  let imageBox = document.querySelector(".img-slider-img");
+  return (imageBox.src = `./assets/imgs/${imgArr[currentImg]}`);
+}
+function nextImgSelct() {
+  if (currentImg >= imgArr.length - 1) {
+    currentImg = -1;
+  }
+  currentImg++;
+  return setImgUI();
+}
+function prevImgSelct() {
+  if (currentImg <= 0) {
+    currentImg = imgArr.length;
+  }
+  currentImg--;
+  return setImgUI();
+}
+
+randImgBtn.addEventListener("click", getRandomImg);
+autoImgBtn.addEventListener("click", autoDisplayImg);
+stopImgBtn.addEventListener("click", stopAutoPlayImg);
+nextImgBtn.addEventListener("click", nextImgSelct);
+prevImgBtn.addEventListener("click", prevImgSelct);
+
+// /////////
+
+// //////////// end   edit task 2---------
+
+// //////////// start task task week 3---------
+
 let apiTitle = document.querySelector(".img-slider-h3-title");
 let apiPost = document.querySelector(".img-slider-p");
 let apispanId = document.querySelector(".apiContent span");
 let apiContent = document.querySelector(".apiContent");
 
-// //////////// start task week 3 ---------
 let apiData;
 let apiCount = 0;
 let url = "https://jsonplaceholder.typicode.com/posts";
@@ -65,56 +131,4 @@ let handlResponsFetch = async () => {
 
 handlResponsFetch();
 
-// let autoPlay;
-// function getRandomImg() {
-//   function randomImg() {
-//     let randapi = Math.floor(Math.random() * el.id + 1);
-//     console.log(randapi);
-//     apiContentContainer.insertAdjacentHTML("afterend", randapi);
-//   }
-
-//   autoPlay = setInterval(randomImg, 900);
-// }
-
-// let currentImg = 0;
-
-// function autoDisplayImg() {
-//   autoPlay = setInterval(() => {
-//     if (currentImg >= imgArr.length - 1) {
-//       currentImg = -1;
-//     }
-//     currentImg++;
-//     return setImgUI();
-//   }, 1000);
-// }
-
-// function stopAutoPlayImg() {
-//   clearInterval(autoPlay);
-// }
-
-// function setImgUI() {
-//   return (imageBox.src = `./assets/imgs/${imgArr[currentImg]}`);
-// }
-
-// function nextImgSelct() {
-//   if (currentImg >= imgArr.length - 1) {
-//     currentImg = -1;
-//   }
-//   currentImg++;
-//   return setImgUI();
-// }
-
-// function prevImgSelct() {
-//   if (currentImg <= 0) {
-//     currentImg = imgArr.length;
-//   }
-//   currentImg--;
-//   return setImgUI();
-// }
-// randImgBtn.addEventListener("click", getRandomImg);
-// autoImgBtn.addEventListener("click", autoDisplayImg);
-// stopImgBtn.addEventListener("click", stopAutoPlayImg);
-// nextImgBtn.addEventListener("click", nextImgSelct);
-// prevImgBtn.addEventListener("click", prevImgSelct);
-
-// //////////// end task week 3 ---------
+// /////////////////////////////////////////////////////////////////////task 3 end
