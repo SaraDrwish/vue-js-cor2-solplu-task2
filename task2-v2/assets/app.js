629411b70987d5a8a58da56546939737b8cdc671
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2024-03-21 07:47:47 +0300

    pl -  task week 3 - overdo  2

--- task2-v2/assets/app.js
+++ task2-v2/assets/app.js
@@ -6,98 +6,115 @@ let stopImgBtn = document.querySelector(".stop");
 
 let apiTitle = document.querySelector(".img-slider-h3-title");
 let apiPost = document.querySelector(".img-slider-p");
-let apispanNum = document.querySelector(".apiContent span");
+let apispanId = document.querySelector(".apiContent span");
 let apiContent = document.querySelector(".apiContent");
 
 // //////////// start task week 3 ---------
 let apiData;
 let apiCount = 0;
+let url = "https://jsonplaceholder.typicode.com/posts";
 
-let handlResponsFetch = () => {
-  fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((res) => {
-      console.log("the res is : " + res);
-      return res.json();
-    })
-    .then((data) => {
-      console.log("data is : (array) ");
+let handlResponsFetch = async () => {
+  try {
+    let response = await fetch(url);
+    let data = await response.json();
+
+    if (response.status == 200) {
       console.log(data);
-      apiData = data.forEach((el) => {
-        apiTitle = `<span>${el.id}</span>`;
-        let apih3 = `<h3>${el.title}</h3>`;
-        let apiP = `<p>${el.body}</p>`;
+      // data.map((d) => {
+      //   console.log("ddattaa : will appear 100 time");
+      //   console.log(d);
+      // });
+    } else {
+      console.log("this is the server error", data);
+    }
+    // console.log("the data is : ", data);
+  } catch (error) {
+    console.log("Fetch error coming from : catch error is : ", error);
+  }
+  // console.log(data[0].title);
+  // console.log(data[0].id);
+  // console.log(data[1].title);
+  // console.log(data[1].id);
 
-        let apiSpanElm = document
-          .querySelector(".apiContent")
-          .insertAdjacentHTML("beforeend", apiP);
-        let apih3Elm = document
-          .querySelector(".apiContent")
-          .insertAdjacentHTML("beforeend", apih3);
+  // .then((data) => {
+  //   console.log("data is : (array) ");
+  //   console.log(data);
+  //   apiData = data.forEach((el) => {
+  //     apiTitle = `<span>${el.id}</span>`;
+  //     let apih3 = `<h3>${el.title}</h3>`;
+  //     let apiP = `<p>${el.body}</p>`;
 
-        let apiTitleElm = document
-          .querySelector(".apiContent")
-          .insertAdjacentHTML("beforeend", apiTitle);
+  //     let apiSpanElm = document
+  //       .querySelector(".apiContent")
+  //       .insertAdjacentHTML("beforeend", apiP);
+  //     let apih3Elm = document
+  //       .querySelector(".apiContent")
+  //       .insertAdjacentHTML("beforeend", apih3);
 
-        console.log(el);
-        console.log(el.body + "   --  " + el.id + "   --  " + el.title);
-        // ////////////
-      });
-    })
-    .catch((error) => console.log("error is : " + error));
+  //     let apiTitleElm = document
+  //       .querySelector(".apiContent")
+  //       .insertAdjacentHTML("beforeend", apiTitle);
+
+  //     console.log(el);
+  //     console.log(el.body + "   --  " + el.id + "   --  " + el.title);
+  //    });
+  // })
+  // .catch((error) => console.log("error is : " + error));
 };
 
 handlResponsFetch();
 
-let autoPlay;
-function getRandomImg() {
-  function randomImg() {
-    let randapi = Math.floor(Math.random() * el.id + 1);
-    console.log(randapi);
-    apiContentContainer.insertAdjacentHTML("afterend", randapi);
-  }
+// let autoPlay;
+// function getRandomImg() {
+//   function randomImg() {
+//     let randapi = Math.floor(Math.random() * el.id + 1);
+//     console.log(randapi);
+//     apiContentContainer.insertAdjacentHTML("afterend", randapi);
+//   }
 
-  autoPlay = setInterval(randomImg, 900);
-}
+//   autoPlay = setInterval(randomImg, 900);
+// }
 
-let currentImg = 0;
+// let currentImg = 0;
 
-function autoDisplayImg() {
-  autoPlay = setInterval(() => {
-    if (currentImg >= imgArr.length - 1) {
-      currentImg = -1;
-    }
-    currentImg++;
-    return setImgUI();
-  }, 1000);
-}
+// function autoDisplayImg() {
+//   autoPlay = setInterval(() => {
+//     if (currentImg >= imgArr.length - 1) {
+//       currentImg = -1;
+//     }
+//     currentImg++;
+//     return setImgUI();
+//   }, 1000);
+// }
 
-function stopAutoPlayImg() {
-  clearInterval(autoPlay);
-}
+// function stopAutoPlayImg() {
+//   clearInterval(autoPlay);
+// }
 
-function setImgUI() {
-  return (imageBox.src = `./assets/imgs/${imgArr[currentImg]}`);
-}
+// function setImgUI() {
+//   return (imageBox.src = `./assets/imgs/${imgArr[currentImg]}`);
+// }
 
-function nextImgSelct() {
-  if (currentImg >= imgArr.length - 1) {
-    currentImg = -1;
-  }
-  currentImg++;
-  return setImgUI();
-}
+// function nextImgSelct() {
+//   if (currentImg >= imgArr.length - 1) {
+//     currentImg = -1;
+//   }
+//   currentImg++;
+//   return setImgUI();
+// }
 
-function prevImgSelct() {
-  if (currentImg <= 0) {
-    currentImg = imgArr.length;
-  }
-  currentImg--;
-  return setImgUI();
-}
-randImgBtn.addEventListener("click", getRandomImg);
-autoImgBtn.addEventListener("click", autoDisplayImg);
-stopImgBtn.addEventListener("click", stopAutoPlayImg);
-nextImgBtn.addEventListener("click", nextImgSelct);
-prevImgBtn.addEventListener("click", prevImgSelct);
+// function prevImgSelct() {
+//   if (currentImg <= 0) {
+//     currentImg = imgArr.length;
+//   }
+//   currentImg--;
+//   return setImgUI();
+// }
+// randImgBtn.addEventListener("click", getRandomImg);
+// autoImgBtn.addEventListener("click", autoDisplayImg);
+// stopImgBtn.addEventListener("click", stopAutoPlayImg);
+// nextImgBtn.addEventListener("click", nextImgSelct);
+// prevImgBtn.addEventListener("click", prevImgSelct);
 
 // //////////// end task week 3 ---------
